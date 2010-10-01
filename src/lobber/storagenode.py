15d84ab7ad17ae1fda86c4a0ec20cc28137f39c3
Leif Johansson <leifj@sunet.se>
2010-10-01 09:08:38 +0200

    implement removal of torrents when done

--- src/lobber/storagenode.py
+++ src/lobber/storagenode.py
@@ -6,79 +6,219 @@ from stompservice import StompClientFactory
 import os,feedparser,json
 from BitTorrent.bencode import bdecode, bencode
 from hashlib import sha1
-from pprint import pprint
 import transmissionrpc
 from urlparse import urlparse
+from tempfile import NamedTemporaryFile
+from deluge.metafile import make_meta_file
+import shutil
+import errno
 
-def _torrent_info(data):
+def decode_torrent(data):
     """
     Return (name, hash) of torrent file.
     """
     info = bdecode(data)['info']
     return info['name'], sha1(bencode(info)).hexdigest()
 
-class TransmissionURLHandler:
+def read_torrent(torrent_file):
+    tfile = file(torrent_file)
+    return decode_torrent(tfile.read())
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: 
+            log.err()
+            raise
+
+def ignore(*args,**kwargs):
+    return
+
+class LobberClient:
     
-    def __init__(self,torrent_dir=None,downloads_dir="/var/lib/transmission-daemon/downloads",rpcurl="http://transmission:transmission@localhost:9091",lobber_key=None):
-        self.torrent_dir = torrent_dir
-        self.downloads_dir = downloads_dir
-        self.rpc = urlparse(rpcurl)
+    def __init__(self,lobber_url="http://localhost:8000",lobber_key=None,torrent_dir="/tmp/lobber-torrents",announce_url=None):
+        self.lobber_url = lobber_url
         self.lobber_key = lobber_key
-    
-    def add_torrent(self,path,info_hash):
-        if path is not None:
-            tc = transmissionrpc.Client(address=self.rpc.hostname,
+        self.torrent_dir = torrent_dir
+        self.announce_url = announce_url
+        mkdir_p(self.torrent_dir)
+        
+    def make_torrent(self,datapath,name=None,comment=None,expires=None):
+        tmptf = NamedTemporaryFile()
+        datafile = file(datapath)
+        if comment == None:
+            comment = "%s" % name
+        if name == None:
+            name = datafile.name
+        
+        make_meta_file(name,self.announce_url, 2**18, comment=comment, target=tmptf)
+        torrent_file = tmptf.name
+        
+        return read_torrent(torrent_file), torrent_file
+
+    def torrent_url(self,identity):
+        return '%s/torrent/%d.torrent' % (self.lobber_url, identity)
+
+    def json_decode(self,data):
+        return json.loads(data)
+
+    def url(self,path):
+        u = "%s%s" % (self.lobber_url,path)
+        return u.encode('ascii')
+
+    def api_call(self,urlpath,page_handler=ignore,*args,**kwargs):
+        r = RetryingCall(client.getPage,self.url(urlpath),agent="Lobber Storage Node/1.0",headers={'X_LOBBER_KEY': self.lobber_key})
+        d = r.start(failureTester=TwitterFailureTester())
+        d.addCallback(self.json_decode)
+        d.addCallback(page_handler,args,kwargs)
+        return
+
+class TransmissionClient:
+    def __init__(self,rpcurl="http://transmission:transmission@localhost:9091",downloads_dir="/var/lib/transmission-daemon/downloads"):
+        self.rpc = urlparse(rpcurl)
+        self.downloads_dir = downloads_dir
+        
+    def client(self):
+        return transmissionrpc.Client(address=self.rpc.hostname,
                                         port=self.rpc.port,
                                         user=self.rpc.username,
                                         password=self.rpc.password)
-            directory = path+os.pathsep+info_hash
-            os.mkdir(directory)
-            #os.chown(directory, 'debian-transmission', 'debian-transmission')
-            tc.add_uri(path,download_dir=directory)
-            
+    
+    def unique_path(self,info_hash):
+        return "%s%s%s" % (self.downloads_dir,os.sep,info_hash)
+    
+    def download(self,torrent_file,directory=None):
+        if torrent_file == None:
+            raise Exception("No torrent file provided")
+        
+        torrent_name,info_hash = read_torrent(torrent_file)  
+        if directory == None:  
+            directory = self.unique_path(info_hash)
+            mkdir_p(directory)
+        
+        tc = self.client()
+        status = None
+        try:
+            status = tc.add_uri(torrent_file,download_dir=directory)
+        except transmissionrpc.transmission.TransmissionError,msg:
+            status = msg
+            log.msg(msg)
+            pass
+        return torrent_name, info_hash, directory, status
+    
+    def upload(self,torrent_file,datapath,move=True):
+        if torrent_file == None:
+            raise Exception("No torrent file provided")
+        
+        torrent_name, info_hash = read_torrent(torrent_file)
+        if move:
+            dst = self.unique_path(info_hash)
+            if not os.path.isdir(dst):
+                mkdir_p(dst)
+            shutil.move(datapath, "%s%s%s" % (dst,os.sep,torrent_name))
+        else:
+            datapath_parts = datapath.split(os.sep)
+            filename = datapath_parts.pop()
+            parent = os.sep.join(datapath_parts)
+            if filename != torrent_name:
+                os.rename(datapath, "%s%s%s" % (parent,os.sep,torrent_name))
+            dst = parent
+
+        tc = self.client()
+        status = None
+        try:
+            status = tc.add_uri(torrent_file,download_dir=dst)
+        except transmissionrpc.transmission.TransmissionError,msg:
+            status = msg
+            log.msg(msg)
+            pass
+        return torrent_name, info_hash, dst, status
+
+class TransmissionSweeper:
+    def __init__(self,lobber,transmission, remove_limit=0):
+        self.transmission = transmission
+        self.lobber = lobber
+        self.remove_limit = remove_limit
+    
+    def remove_if_done(self,r,args,kwargs):
+        if int(self.remove_limit) <= int(r['count']):
+            log.msg("Removing torrent %d" % args[0])
+            tc = self.transmission.client()
+            tc.remove(args[0],delete_data=True)
+    
+    def check_done(self):
+        tc = self.transmission.client()
+        for t in tc.list().values():
+            log.msg("[%d] %s %s %s" % (t.id,t.hashString,t.name,t.status))
+            tc.start(t.id)
+            tc.change(t.id,seedRatioMode=2,uploadLimited=False,downloadLimited=False)
+            if t.status == 'seeding':
+                self.lobber.api_call("/torrent/ihave/%s" % t.hashString)
+                if self.remove_limit > 0:
+                    self.lobber.api_call("/torrent/hazcount/%s" % t.hashString, self.remove_if_done,t.id)
+                
+class TransmissionURLHandler:
+    
+    def __init__(self,lobber,transmission):
+        self.transmission = transmission
+        self.lobber = lobber
+    
+    def torrent_file(self,info_hash):
+        return self.lobber.torrent_dir+os.sep+info_hash+".torrent"
     
     def handle_page(self,data):
         if data.startswith("d8:announce"):
-            (name,info_hash) = _torrent_info(data)
+            (name,info_hash) = decode_torrent(data)
             log.msg("Got torrent with info_hash "+info_hash)
-            fn = self.torrent_dir+os.sep+info_hash+".torrent"
+            fn = self.torrent_file(info_hash)
             if not os.path.exists(fn):
                 f = open(fn,"w")
                 f.write(data)
                 f.close()
                 log.msg("Wrote torrent with info_hash "+info_hash+" to file "+fn)
-                self.add_torrent(fn,info_hash)
-        
-        if "<rss" in data:
+                self.transmission.download(fn)
+        elif "<rss" in data:
             try:
                 f = feedparser.parse(data)
                 for e in f.entries:
-                    self.load_url_retry(e.link.encode('ascii'))
+                    self.load_url_retry(e.link)
+            except Exception,e:
+                log.msg(e)
+        else:
+            try:
+                torrents = json.loads(data)
+                for t in torrents:
+                    fn = self.torrent_file(t['info_hash'])
+                    if not os.path.exists(fn):
+                        log.msg("adding %s from %s" % (t['label'],fn))
+                        self.load_url_retry("%s%s.torrent" % (self.lobber.lobber_url,t['link']))
             except Exception,e:
                 log.msg(e)
             
         return
     
     def load_url(self,url):
-        d = client.getPage(url,agent="Lobber Storage Node/1.0",headers={'X_LOBBER_KEY': self.lobber_key})
+        d = client.getPage(url.encode('ascii'),agent="Lobber Storage Node/1.0",headers={'X_LOBBER_KEY': self.lobber.lobber_key})
         d.addCallback(self.handle_page)
         return
     
     def load_url_retry(self,url):
-        r = RetryingCall(client.getPage,url,agent="Lobber Storage Node/1.0",headers={'X_LOBBER_KEY': self.lobber_key})
+        r = RetryingCall(client.getPage,url.encode('ascii'),agent="Lobber Storage Node/1.0",headers={'X_LOBBER_KEY': self.lobber.lobber_key})
         d = r.start(failureTester=TwitterFailureTester())
         d.addCallback(self.handle_page)
         return
 
 class TorrentDownloader(StompClientFactory):
 
-    def __init__(self,
-                 destinations=["/torrent/new"],
-                 url_handler=TransmissionURLHandler("/tmp"),
-                 lobber_url="http://localhost:8080"):
+    def __init__(self,lobber,transmission,destinations=["/torrent/add"]):
         self.destinations = destinations
-        self.url_handler = url_handler
-        self.lobber_url = lobber_url
+        self.url_handler = TransmissionURLHandler(lobber, transmission)
+        self.lobber = lobber
+        self.transmission = transmission
 
     def recv_connected(self, msg):
         for dst in self.destinations:
@@ -91,5 +231,4 @@ class TorrentDownloader(StompClientFactory):
             log.msg("Got an unknown message")
             return
              
-        url = '%s/torrent/%d.torrent' % (self.lobber_url, identity)
-        self.url_handler.load_url_retry(url)
+        self.url_handler.load_url_retry(self.lobber.torrent_url(identity))
--- src/twisted/plugins/lobberstoragenode_plugin.py
+++ src/twisted/plugins/lobberstoragenode_plugin.py
@@ -2,14 +2,15 @@ from twisted.application import service
 from twisted.python import log
 from twisted.application import internet
 
-from lobber.storagenode import TransmissionURLHandler, TorrentDownloader
+from lobber.storagenode import TransmissionURLHandler, TorrentDownloader,\
+    LobberClient, TransmissionClient, TransmissionSweeper
 from twisted.python import usage
 import os
 from urlparse import urlparse
 from twisted.internet import task
 from zope.interface.declarations import implements
 from twisted.plugin import IPlugin
-from pprint import pprint
+import sys
 
 class Options(usage.Options):
 
@@ -17,8 +18,11 @@ class Options(usage.Options):
         ["stompUrl", "S", "stomp://localhost:61613","The STOMP protocol URL to use for notifications"],
         ["lobberKey", "k", None, "The Lobber application key to use"],
         ["torrentDir", "d", "torrents", "The directory where to store torrents"],
-        ["lobberUrl", "u", "http://localhost:8000/torrents", "The Lobber URL prefix"],
-        ['lobberHost',"h", None, "The host running both STOMP and https for lobber"]
+        ["lobberUrl", "u", "http://localhost:8000", "The Lobber URL prefix"],
+        ['lobberHost',"h", None, "The host running both STOMP and https for lobber"],
+        ['transmissionRpc','T',"http://transmission:transmission@localhost:9091","The RPC URL for transmission"],
+        ['transmissionDownloadsDir','D',"/var/lib/transmission-daemon/downloads","The downloads directory for transmission"],
+        ['removeLimit','r',0,"Remove torrent and data when this many other storage-nodes have the data (0=never remove)"]
     ]
     
     def parseArgs(self,*args):
@@ -49,23 +53,29 @@ class MyServiceMaker(object):
     tapname = 'lobberstoragenode'
     description = "A Storage Node for Lobber"
     options = Options
+    sweeper = None
+    getter = {}
 
     def makeService(self, options):
         """
         Constructs a lobber storage node service
         """
-        dl = TransmissionURLHandler(torrent_dir=options['torrentDir'].rstrip(os.sep),
-                                    lobber_key=options['lobberKey'])
+        lobber = LobberClient(options['lobberUrl'], options['lobberKey'], options['torrentDir'].rstrip(os.sep))
+        transmission = TransmissionClient(options['transmissionRpc'], options['transmissionDownloadsDir'])
 
-        torrentDownloader = TorrentDownloader(options.destinations,dl,options['lobberUrl'])
+        torrentDownloader = TorrentDownloader(lobber,transmission,options.destinations)
         stompService = internet.TCPClient(options['stomp_host'],options['stomp_port'],torrentDownloader)
         
-        getter = {}
+        self.getter = {}
         for url in options.urls:
-            log.msg("Pulling data from "+url)
-            getter[url] = task.LoopingCall(dl.load_url,url)
-            getter[url].start(10,True)
-            
+            log.msg("Pulling RSS/Torrent from "+url)
+            self.getter[url] = task.LoopingCall(torrentDownloader.url_handler.load_url,url)
+            self.getter[url].start(30,True)
+        
+        transmissionSweeper = TransmissionSweeper(lobber, transmission, remove_limit=options['removeLimit'])
+        self.sweeper = task.LoopingCall(transmissionSweeper.check_done)
+        self.sweeper.start(30,True)
+        
         return stompService
     
 serviceMaker = MyServiceMaker()
